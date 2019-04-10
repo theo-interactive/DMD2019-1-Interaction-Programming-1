49add6c76c9af2d47b2020be7968f5da2cdc902a
asunnyols <asunnyols@naver.com>
2019-04-10 23:28:10 +0900

    김은서 과제제출
    제출합니다!

---
+++ 5Week/21861030_KimEunseo/js/script.js
@@ -0,0 +1,58 @@
+//Q1
+var string='';
+var number='';
+function getEven(num_0){
+    for(var i = 0; i <= num_0; i++){
+        if(i%2==0){
+            string +='짝수'+'';
+            number += i +'';
+        }
+        console.log(string);
+        console.log(number);
+    }
+
+
+even1 = getEven(10);
+
+//Q2
+var getStar = function (num_1){
+    if (typeof num_1 !== typeof 1 ) {
+        console.log("숫자만 입력 가능합니다.");
+        return false;
+
+    }
+    else if (typeof num_1 === typeof 1 ) {
+        var result= '';
+        for (var i = 0; i < num_1; i++) {
+            result += '*';
+        }
+        return result;
+    }
+}
+var star1 = getStar("text");
+var star2 = getStar(5);
+
+console.log(star1);
+console.log(star2); 
+
+
+//Q3
+
+var fruit = ["Apple", "Orange", "Grape", "Melon"];
+fruit.unshift("Mango");
+fruit.splice(3,0,"Cherry");
+
+console.log(fruit.reverse());
+// fruite.reverse();
+// console.log(fruit);
+
+
+//Q4
+
+function getSize(width, height){
+    return ("{winWidth : 윈도우 영역의 넓이값, winHeight : 윈도우 영역의 높이값}")
+}
+var winSize = getSize();
+
+console.log("윈도우 콘텐츠의 영역 width : " + winSize.winWidth + " px, height : " + winSize.winHeight + " px.");
+console.log(winSize);}
