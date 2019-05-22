ff296aeb4cd98ae5b2dd82bdd4f704790b78a005
pidasom <aaafaaa@naver.com>
2019-05-22 17:34:32 +0900

    21761130_피다솜

---
+++ 11Week/21761130_pidasom/script/script.js
@@ -0,0 +1,24 @@
+      var $tabMenu = document.querySelector("#tab-menu");
+      var $tabMenuEl = $tabMenu.querySelectorAll('a'); //List. [... ... ... ...];
+      // console.log($tabMenu, $tabMenuEl);
+      var $tabContent = document.getElementsByClassName('tab-content'); //List. [... ... ... ...];
+      // console.log($tabContent);
+      for(var i = 0; i < $tabMenuEl.length; i++){
+        tabMenuClick(i);
+      }
+      function tabMenuClick(index){
+        //i의 순서를 체크할 수 있게 index의 전달인자로 활당
+        $tabMenuEl[index].addEventListener('click', function(e){
+          e.preventDefault();
+          console.log(index);
+          for(var i = 0; i < $tabMenuEl.length; i++){
+            //모든 요소에서 .selected 를 삭제
+            $tabMenuEl[i].classList.remove('selected');
+            $tabContent[i].classList.remove('selected');
+            if(i === index){
+              $tabMenuEl[i].classList.add('selected');
+              $tabContent[i].classList.add('selected');
+            }
+          }
+        });
+      }
